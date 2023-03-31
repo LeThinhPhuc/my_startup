7c41994f7f7dd09cfde34b19df82515e00f39821
TonyTaii <pinpok65@gmail.com>
2023-03-31 13:30:40 +0700

    Add Admin Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,19 @@ import JobDetail from "./components/JobDetail";
 import Header from "./components/CareerBody/Header/Header";
 import Content from "./components/CareerBody/Content/Content";
 import ApplyPage from "./pages/ApplyPage/ApplyPage";
+import AdminPage from "./pages/AdminPage";
 function App() {
   return (
-    <div className="min-h-screen flex flex-col ml-0">
-      <CareerHeader />
-      <main className="flex-1 w-full">
-        {/* <Header/> */}
-        {/* <Content/> */}
-        <ApplyPage />
-      </main>
-      <CareerFooter />
-    </div>
+    // <div className="min-h-screen flex flex-col ml-0">
+    //   <CareerHeader />
+    //   <main className="flex-1 w-full">
+    //     {/* <Header/> */}
+    //     {/* <Content/> */}
+    //     <ApplyPage />
+    //   </main>
+    //   <CareerFooter />
+    // </div>
+    <AdminPage/>
   );
 }
 
